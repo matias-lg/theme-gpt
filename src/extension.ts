ba54fafa7f3517c8455f93bb541e3111ec85e9c3
Matías López <matiaslg965@gmail.com>
2024-06-18 16:09:24 -0400

    setup openai

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,58 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "theme-gpt" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "theme-gpt" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "theme-gpt.helloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello World from theme-gpt!!!!!");
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('theme-gpt.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from theme-gpt!');
-	});
+  const timeDisposable = vscode.commands.registerCommand(
+    "theme-gpt.time",
+    () => {
+      const time = new Date().toLocaleTimeString();
+      vscode.window.showInformationMessage(`The current time is: ${time}`);
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  const gptDisposable = vscode.commands.registerCommand(
+    "theme-gpt.gpt",
+    async () => {
+      // const completion = await openai.chat.completions.create({
+      //   messages: [{ role: "system", content: "You are a helpful assistant." }],
+      //   model: "gpt-3.5-turbo",
+      // });
+
+      const message = process.env.OPENAI_API_KEY || "cool";
+      //completion.choices[0].message.content
+
+      vscode.window.showInformationMessage(message);
+    }
+  );
+
+  context.subscriptions.push(...[disposable, timeDisposable]);
 }
 
 // This method is called when your extension is deactivated
